1f38a14a914d15b0f0021480991355b76c20ff7d
Righty75 <gmode.samui@gmail.com>
2024-05-09 13:16:23 +0700

    merge test finish

--- Peace/merge/merge_test.py
+++ Peace/merge/merge_test.py
@@ -2,15 +2,24 @@ import pandas as pd
 
 data_name = pd.read_excel('./Peace/sampledata.xlsx','sample_for_merge1')
 data_nation = pd.read_excel('./Peace/sampledata.xlsx','sample_for_merge2')
-data_nation2 = pd.read_excel('./Peace/sampledata.xlsx','sample_for_merge3')
+data_selected_nation = pd.read_excel('./Peace/sampledata.xlsx','sample_for_merge3')
 
 ##เลือกเฉพาะข้อมูลจากด้านซ้าย
-#print(data_name.merge(data_nation, how='left', on='Nationality'))
+left_result = data_name.merge(data_selected_nation, how='left', on='Nationality')
+#print(left_result)
 
 ##เลือกเฉพาะข้อมูลจากด้านขวา
-print(data_name.merge(data_nation, how='right', on='Nationality')[0:30])
+right_result = data_name.merge(data_nation, how='right', on='Nationality')
+#print(right_result)
 
 ##เลือกเฉพาะข้อมูลที่ตรงกันทั้ง 2 ฝั่ง
-#print(data_name.merge(data_nation, how='inner', on='Nationality'))
+both_result = data_name.merge(data_nation, how='inner', on='Nationality')
+#print(both_result)
 
 ##เลือกเฉพาะข้อมูลที่ไม่ตรงกับด้านขวา
+excludeB = left_result[left_result['Alpha code'].isna()]
+#print(excludeB)
+
+##เลือกเฉพาะข้อมูลที่ไม่ตรงกับด้านซ้าย
+excludeA = right_result[right_result['Name'].isna()]
+print(excludeA)
