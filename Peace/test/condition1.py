ca493573813b1324a8476523f9f6a3b19d7eb880
Righty75 <gmode.samui@gmail.com>
2024-05-13 14:33:48 +0700

    condition1 G/L config

--- Peace/test/condition1.py
+++ Peace/test/condition1.py
@@ -4,8 +4,11 @@ dataframe = pd.read_csv('.\Peace\EXP_DATA_30042024.csv')
 dataframe['G/L'] = dataframe['G/L'].apply(str)
 condition1 = pd.read_excel('.\Peace\condition.xlsx')
 
+#ลบตัว X ออกจากเงื่อนไขรหัสที่ 1
+condition1 = condition1.iloc[0,0].replace('X','')
+
 #เลือกรหัส 516017XX
-filtered = dataframe.loc[dataframe['G/L'].str.contains('^516017\d{2}$', regex=True)]
+filtered = dataframe.loc[dataframe['G/L'].str.contains(('^'+condition1+'\d{2}$'), regex=True)]
 
 #นำ record ที่ไม่มีรหัสกิจกรรม R, N, E ออกมา
 NotRNE = filtered.loc[~filtered['Bus. Process'].str.contains('[RNE]{1}[A-Z0-9]{4}$', na=False, regex=True)]
