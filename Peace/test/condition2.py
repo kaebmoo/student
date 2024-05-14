d60757016a5ee8a46311e4ad891af851ce961f13
Righty75 <gmode.samui@gmail.com>
2024-05-14 10:14:32 +0700

    เงื่อนไข 5-6 + config

--- Peace/test/condition2.py
+++ Peace/test/condition2.py
@@ -4,11 +4,11 @@ dataframe = pd.read_csv('.\Peace\EXP_DATA_30042024.csv')
 dataframe['G/L'] = dataframe['G/L'].apply(str)
 all_condition = pd.read_excel('.\Peace\condition.xlsx')
 
-#ลบตัว X ออกจากเงื่อนไขรหัสที่ 2
-condition2 = all_condition.iloc[1,0].replace('X','')
+#เปลี่ยนตัว X เป็น \d จากเงื่อนไขรหัสที่ 2 เพื่อไปใช้ใน regex
+condition2 = all_condition.iloc[1,0].replace('X','\d')
 
 #เลือกรหัส 536017XX
-filtered = dataframe.loc[dataframe['G/L'].str.contains(('^'+condition2+'\d{2}$'), regex=True)]
+filtered = dataframe.loc[dataframe['G/L'].str.contains((condition2), regex=True)]
 
 #นำ record ที่ไม่มีรหัสกิจกรรม N ออกมา
 NotN = filtered.loc[~filtered['Bus. Process'].str.contains('[N]{1}[A-Z0-9]{4}$', na=False, regex=True)]
--- Peace/test/condition4.py
+++ Peace/test/condition4.py
@@ -4,9 +4,11 @@ dataframe = pd.read_csv('.\Peace\EXP_DATA_30042024.csv')
 dataframe['G/L'] = dataframe['G/L'].apply(str)
 all_condition = pd.read_excel('.\Peace\condition.xlsx')
 
+#เปลี่ยนตัว X เป็น \d จากเงื่อนไขรหัสที่ 4 เพื่อไปใช้ใน regex
+condition4 = all_condition.iloc[3,0].replace('X','\d')
+
 #เลือกรหัส 5X602XXX
-filtered = dataframe.loc[dataframe['G/L'].str.contains(('5\d602\d\d\d'), regex=True)]
-print(filtered)
+filtered = dataframe.loc[dataframe['G/L'].str.contains((condition4), regex=True)]
 
 #นำ record ที่ไม่มีรหัสกระบวนการทางธุรกิจ ออก
 NoBP = filtered[filtered['Bus. Process'].isna()]
