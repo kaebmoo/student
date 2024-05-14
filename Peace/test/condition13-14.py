cc0edfd07b64214344bc5c7017e95622c81228cc
Righty75 <gmode.samui@gmail.com>
2024-05-14 15:18:22 +0700

    รวม condition, เงื่อนไข 15-19

--- Peace/test/condition13-14.py
+++ Peace/test/condition13-14.py
@@ -6,8 +6,6 @@ all_condition = pd.read_excel('.\Peace\condition.xlsx')
 cancel_number = pd.read_excel('.\Peace\condition.xlsx', 'รหัสกิจกรรมยกเลิก')
 cancel_number = cancel_number['Act']
 
-condition_13 = all_condition.iloc[12,0]
-
 filtered = dataframe.loc[(dataframe['Bus. Process'].str.contains(cancel_number[39], na=False)) | (dataframe['Bus. Process'].str.contains(cancel_number[40], na=False))]
 filtered.to_csv('./Peace/test/result/Cancel_number.csv')
 
---
+++ Peace/test/condition4-6.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+dataframe = pd.read_csv('.\Peace\EXP_DATA_30042024.csv')
+dataframe['G/L'] = dataframe['G/L'].apply(str)
+all_condition = pd.read_excel('.\Peace\condition.xlsx')
+
+#เปลี่ยนตัว X เป็น \d จากเงื่อนไขรหัสที่ 4 เพื่อไปใช้ใน regex
+condition4 = all_condition.iloc[3,0].replace('X','\d')
+
+#เลือกรหัส 5X602XXX
+filtered = dataframe.loc[dataframe['G/L'].str.contains((condition4), regex=True)]
+
+#นำ record ที่ไม่มีรหัสกระบวนการทางธุรกิจ ออก
+NoBP = filtered[filtered['Bus. Process'].isna()]
+NoBP.to_csv('./Peace/test/result/NoBP_ค่าสวัสดิการ.csv')
+
+#-------------------------------------------------------------------------------------------------#
+
+condition5 = all_condition.iloc[4,0].replace('X','\d')
+
+#เลือกรหัส 5X603XXX
+filtered = dataframe.loc[dataframe['G/L'].str.contains((condition5), regex=True)]
+
+#นำ record ที่ไม่มีรหัสกระบวนการทางธุรกิจ ออก
+NoBP = filtered[filtered['Bus. Process'].isna()]
+NoBP.to_csv('./Peace/test/result/NoBP_ฝึกอบรมบุคลากร.csv')
+
+#-------------------------------------------------------------------------------------------------#
+
+condition6 = all_condition.iloc[5,0].replace('X','\d')
+
+#เลือกรหัส 5X604XXX
+filtered = dataframe.loc[dataframe['G/L'].str.contains((condition6), regex=True)]
+
+#นำ record ที่ไม่มีรหัสกระบวนการทางธุรกิจ ออก
+NoBP = filtered[filtered['Bus. Process'].isna()]
+NoBP.to_csv('./Peace/test/result/NoBP_เดินทางเพื่อปฏิบัติงาน.csv')
