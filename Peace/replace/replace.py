333de75d404257f13a843c03a9cb00d9ef87ed08
Righty75 <gmode.samui@gmail.com>
2024-06-05 15:01:05 +0700

    update replace function

--- Peace/replace/replace.py
+++ Peace/replace/replace.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-df = pd.read_excel('./Peace/test/PACKAGE_CATEGORY.xlsx', dtype=str)
+df = pd.read_excel('./Peace/replace/PACKAGE_CATEGORY.xlsx', dtype=str)
 
-df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.replace(r'^[A-Z0-9]+ : ', '', regex=True)
+df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.replace(r'^[A-Z0-9]+ :', '', regex=True)
+df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.replace(r'ค่าบริการ', '', regex=True)
+df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.strip()
 print(df)
 
-df.to_excel('./Peace/test/result.xlsx', index=False)
+df.to_excel('./Peace/replace/result.xlsx', index=False)
