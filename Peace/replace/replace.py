9c39e73185fa2f39c8a8dd5b20bc3f281c56f8da
Righty75 <gmode.samui@gmail.com>
2024-06-06 09:24:42 +0700

    Update replace.py

--- Peace/replace/replace.py
+++ Peace/replace/replace.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-df = pd.read_excel('./Peace/replace/PACKAGE_CATEGORY.xlsx', dtype=str)
+df = pd.read_csv('./Peace/replace/CONTRACT_PACKAGE_NAME.csv', dtype=str)
 
-df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.replace(r'^[A-Z0-9]+\s*:\s*', '', regex=True)
+print(df)
+df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.replace(r'^\s*[A-Z0-9_]+(?:\s+[A-Z0-9_]+)?(?:\s*\([^)]+\))?(\s*:\s*|\s*:)', '', regex=True)
 df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.replace(r'ค่าบริการ', '', regex=True)
 df['CONTRACRT NAME TH'] = df['CONTRACRT NAME TH'].str.strip()
 print(df)
 
-df.to_excel('./Peace/replace/result.xlsx', index=False)
+df.to_csv('./Peace/replace/CONTRACT_PACKAGE_NAME_20240605.csv', index=False)
